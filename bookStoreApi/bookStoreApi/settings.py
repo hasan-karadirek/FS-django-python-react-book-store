2f8c035aaf74e66de85a6a77d00c6ecab5b4f85c
HackYourFuture <hackyourfuture@HackYourFutures-MacBook-Air.local>
2024-01-21 22:06:54 +0100

    customer register endpoind created

--- bookStoreApi/bookStoreApi/settings.py
+++ bookStoreApi/bookStoreApi/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'store',
+    'customer',
 ]
 
 MIDDLEWARE = [
@@ -87,7 +88,6 @@ DATABASES = {
     }
 }
 
-
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
@@ -129,3 +129,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL="customer.Customer"
---
+++ bookStoreApi/customer/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+from .models import Customer
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Customer
+        fields = ('username', 'email', 'password')  
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = Customer.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password']
+        )
+        return user
