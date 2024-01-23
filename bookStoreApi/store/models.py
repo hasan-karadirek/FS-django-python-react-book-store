2b50990507b7fc7b83edd32fbca3c55888531e1f
HackYourFuture <hackyourfuture@HackYourFutures-MacBook-Air.local>
2024-01-23 23:09:06 +0100

    bookOnSale models created

--- bookStoreApi/store/models.py
+++ bookStoreApi/store/models.py
@@ -3,24 +3,25 @@ from book.models import Book
 from customer.models import Customer
 class BookOnSale(models.Model):
     conditionChoices = [
-        ("new", 'New'),
-        ("likeNew", 'Like New'),
-        ("good", 'Good'),
-        ("poor", 'Poor'),
+    ("NEW", 'New'),
+    ("LIKE_NEW", 'Like New'),
+    ("GOOD", 'Good'),
+    ("POOR", 'Poor'),
     ]
+
     book=models.ForeignKey(Book, on_delete=models.CASCADE)
     customer=models.ForeignKey(Customer, on_delete=models.CASCADE)
-    condition=models.CharField(max_length=50,choices=conditionChoices)
-    price=models.DecimalField()
+    condition=models.CharField(max_length=50,choices=conditionChoices,verbose_name="Condition of the Book")
+    price=models.DecimalField(max_digits=10, decimal_places=2)
     description=models.CharField(max_length=500)
 
     def __str__(self):
         return f'{self.book.name} - {self.customer.username}'  
 class BookOnSaleImage(models.Model):
-    book = models.ForeignKey(BookOnSale, related_name='onSale_images', on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='onSale_images/',max_length=1000000)  
+    book = models.ForeignKey(BookOnSale, related_name='on_sale_images', on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='on_sale_images/',max_length=1000000)  
 
     def __str__(self):
-        return f'{self.book.name} - {self.image.url}'  
+        return f'{self.book.book.name} - {self.image.url}'  
     
 
