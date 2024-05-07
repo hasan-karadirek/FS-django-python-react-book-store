9fe9f46832213c6c39a036db8ed53cd610c3d2ff
HasanK <hasankaradirek3@gmail.com>
2024-05-07 17:28:14 +0200

    book model changed

--- bookStoreApi/book/models.py
+++ bookStoreApi/book/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.conf import settings
-
+import datetime
 
 # Author Model
 class Author(models.Model):
@@ -16,17 +16,73 @@ class PublishingHouse(models.Model):
 
     def __str__(self):
         return self.name
-
-
-# Book Model
-class Book(models.Model):
-    ean = models.CharField(max_length=13, unique=True)
-    name = models.CharField(max_length=255)
-    author = models.ForeignKey(Author, on_delete=models.CASCADE)
-    publishing_house = models.ForeignKey(PublishingHouse, on_delete=models.CASCADE)
+class Language(models.Model):
+    name=models.CharField(max_length=255)
 
     def __str__(self):
         return self.name
+class Tag(models.Model):
+    name=models.CharField(max_length=255)
+    def __str__(self):
+        return self.name
+class Category(models.Model):
+    title=models.CharField(max_length=255, unique=True)
+    
+    def __str__(self):
+        return self.title
+
+# Book Model
+class Book(models.Model):
+    coverChoices = [
+        ("PAPERBACK", "Paperback"),
+        ("HARDCOVER", "Hardcover"),
+        ("POCKET", "Pocket")
+    ]
+    conditionChoices = [
+        ("NEW", "New"),
+        ("LIKE_NEW", "Like New"),
+        ("GOOD", "Good"),
+        ("REASONABLE", "Reasonable"),
+    ]
+    statusChoices = [
+        ("OPEN", "Open"),
+        ("SOLD", "Sold"),
+        ("PENDING", "Pending"),
+        ("DRAFT", "Draft"),
+    ]
+    Isbn = models.CharField(max_length=20,null=True)
+    env_no=models.IntegerField(unique=True)
+    title = models.CharField(max_length=255)
+    author = models.ForeignKey(Author,related_name="author")
+    language=models.ForeignKey(Language,related_name="language")
+    cover= models.CharField(
+        max_length=50,
+        choices=coverChoices,
+        verbose_name="Cover type of the book",
+    )
+    publishing_house = models.ForeignKey(PublishingHouse)
+    year=models.IntegerField(min=1700,max=datetime.date.today().year)
+    edition=models.CharField(max_length=255)
+    category=models.ForeignKey(Category,related_name="category")
+    condition_description=models.CharField(max_length=255)
+    condition=models.CharField(
+        max_length=50,
+        choices=conditionChoices,
+        verbose_name="Condition of the book",
+    )
+    tags=models.ForeignKey(Tag, related_name="tag")
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    entry=models.DateField(default=datetime.now(datetime.timezone.utc))
+    status=models.CharField(
+        max_length=50,
+        choices=statusChoices,
+        verbose_name="Status of the book",
+    )
+
+
+
+    def __str__(self):
+        return f"{self.title} - {self.author.name} - {self.year}"
 
 
 class BookImage(models.Model):
@@ -34,4 +90,4 @@ class BookImage(models.Model):
     image = models.ImageField(upload_to="book_images/")
 
     def __str__(self):
-        return f"{self.book.name} - {self.image.url}"
+        return f"{self.book.title} - {self.image.url}"
