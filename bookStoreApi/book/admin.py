2e77d08f6a143f575c91ee9280410c35f7449b01
HasanK <hasankaradirek3@gmail.com>
2024-05-10 21:15:24 +0200

    add-to-cart, remove-from-cart, checkout logics refactored

--- bookStoreApi/book/admin.py
+++ bookStoreApi/book/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from .models import Book, BookTagAssociation, BookImage
+from .models import Book, BookTagAssociation, BookImage,Author,Language,Category,PublishingHouse
 
 admin.site.register(Book)
 admin.site.register(BookTagAssociation)
 admin.site.register(BookImage)
+admin.site.register(Author)
+admin.site.register(PublishingHouse)
+admin.site.register(Category)
+admin.site.register(Language)
 
--- bookStoreApi/store/serializers.py
+++ bookStoreApi/store/serializers.py
@@ -1,51 +1,17 @@
 from rest_framework import serializers
-from .models import BookOnSale, BookOnSaleImage, OrderDetail, Order
-from book.models import Book
+from .models import OrderDetail, Order
+from book.serializers import BookSerializer
 from core.custom_exceptions import CustomAPIException
 
 
-class BookOnSaleImageSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = BookOnSaleImage
-        fields = ["book", "image"]
-
-
-class BookOnSaleSerializer(serializers.ModelSerializer):
-    book = serializers.CharField(max_length=50)
-    on_sale_images = BookOnSaleImageSerializer(many=True, read_only=True)
-    images = serializers.ListField(
-        child=serializers.ImageField(max_length=255), write_only=True
-    )
-
-    class Meta:
-        model = BookOnSale
-        fields = ["id", "book", "condition", "price", "images", "on_sale_images"]
-
-    def create(self, validated_data):
-        images_data = validated_data.pop("images")
-        bookId = validated_data.pop("book")
-        customer = self.context["request"].user
-        try:
-            book = Book.objects.get(id=bookId)
-        except Book.DoesNotExist:
-            raise CustomAPIException("A book with this ID does not exist.", status=404)
-
-        on_sale_book = BookOnSale.objects.create(
-            book=book, customer=customer, **validated_data
-        )
-
-        for image_data in images_data:
-            BookOnSaleImage.objects.create(book=on_sale_book, image=image_data)
-
-        return on_sale_book
 
 
 class OrderDetailSerializer(serializers.ModelSerializer):
-    book_on_sale = BookOnSaleSerializer(read_only=True)
+    book = BookSerializer(read_only=True)
 
     class Meta:
         model = OrderDetail
-        fields = ["order", "book_on_sale"]
+        fields = ["id","order", "book"]
 
 
 class OrderSerializer(serializers.ModelSerializer):
