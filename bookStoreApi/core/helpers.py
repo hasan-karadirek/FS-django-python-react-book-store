d5ce29f777c1573f351f18266edf4cb0bd6c284d
HasanK <hasankaradirek3@gmail.com>
2024-05-25 20:45:13 +0200

    isTokenExpired helpers implemented

--- bookStoreApi/core/helpers.py
+++ bookStoreApi/core/helpers.py
@@ -1,5 +1,8 @@
 from django.core.paginator import Paginator, EmptyPage
 from core.custom_exceptions import CustomAPIException
+from rest_framework.authtoken.models import Token
+from django.utils import timezone
+from datetime import timedelta  
 
 
 
@@ -10,3 +13,17 @@ def pagination(instances,size,page_number):
     except EmptyPage:
         raise CustomAPIException("No more pages", status=404)
     return page
+
+
+def isTokenExpired(request):
+    print("heyhey",str(request.user))
+    if str(request.user)!="AnonymousUser":
+        try:
+            print("heyhey",str(request.user))
+            token = Token.objects.get(user=request.user.id)
+            if timezone.now() - token.created > timedelta(hours=12):
+                token.delete()
+                raise CustomAPIException("Token has expired. Please login again.",401)
+        except Token.DoesNotExist:
+            raise CustomAPIException("Invalid Token.",401)
+    
