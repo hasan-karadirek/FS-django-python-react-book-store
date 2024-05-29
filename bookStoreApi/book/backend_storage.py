735d3b755030779b8d3e180493982210b41a674c
HasanK <hasankaradirek3@gmail.com>
2024-05-29 21:38:42 +0200

    boto3 config

--- bookStoreApi/book/backend_storage.py
+++ bookStoreApi/book/backend_storage.py
@@ -2,8 +2,15 @@ from storages.backends.s3boto3 import S3Boto3Storage
 
 class StaticStorage(S3Boto3Storage):
     location = 'static'
-    default_acl = 'public-read'
+    default_acl = None  # Do not set ACLs
+    object_parameters = {
+        'CacheControl': 'max-age=86400',
+    }
 
 class MediaStorage(S3Boto3Storage):
     location = 'media'
+    default_acl = None  # Do not set ACLs
     file_overwrite = False
+    object_parameters = {
+        'CacheControl': 'max-age=86400',
+    }
