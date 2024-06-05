478c6246bb910598f8295419f2cff80c10c0c596
HasanK <hasankaradirek3@gmail.com>
2024-06-05 13:33:18 +0200

    email bug fixed

--- bookStoreApi/react_app/src/components/home/Testimonials.tsx
+++ bookStoreApi/react_app/src/components/home/Testimonials.tsx
@@ -74,11 +74,11 @@ const Testimonials: React.FC = () => {
               <div className="testimonial-carousel-caption carousel-caption  d-md-block">
               <div className="col-md-8 offset-md-2">
                 <div className="testimonial-card">
-                    <p className="quote">Gezellig, geen druk om te kopen. Véél oude boeken. Ik heb
+                    <p className="quote">"Gezellig, geen druk om te kopen. Véél oude boeken. Ik heb
                       zelfs Kruistocht in spijkerbroek gezien! Één van mijn
                       favoriete jeugdboeken. Ook andere talen aanwezig. Ga dat
                       zien! Er zijn helaas niet veel meer van dit soort
-                      tweedehands boekenwinkels.</p>
+                      tweedehands boekenwinkels."</p>
                     <a target="_blank" href="https://www.google.com/search?sa=X&sca_esv=5aa8bd575662fb0a&rlz=1C1GCEA_enNL980NL983&hl=tr-NL&tbm=lcl&sxsrf=ADLYWIKaM-rkdmeukbTCPwOr8gKV28yFPw:1716680700758&q=le%20flaneur%20amsterdam%20yorumlar%C4%B1&rflfq=1&num=20&stick=H4sIAAAAAAAAAONgkxI2NjM1NzQwNrCwMDU1tTAyNbM02cDI-IpRPidVIS0nMS-1tEghMbe4JLUoJTFXoTK_qDQ3J7HoyMZFrIRUAACNOfqfXgAAAA&rldimm=3657103088555825694&ved=0CBAQ5foLahcKEwjAmcGL_qmGAxUAAAAAHQAAAAAQBQ&biw=1775&bih=917&dpr=1#lkt=LocalPoiReviews&arid=ChZDSUhNMG9nS0VJQ0FnSUNwN1p1dllBEAE">
                       Bobje Catootje
                     </a>
@@ -90,11 +90,11 @@ const Testimonials: React.FC = () => {
               <div className="testimonial-carousel-caption carousel-caption  d-md-block">
               <div className="col-md-8 offset-md-2">
                 <div className="testimonial-card">
-                    <p className="quote">Really diverse collection, wonderful vibes ahah, the owner
+                    <p className="quote">"Really diverse collection, wonderful vibes ahah, the owner
                       was super friendly. It felt like I got in a fairy-tale.
                       All books are unexpectedly affordable, I got Kafka for
                       just 8 euro, but some other (known) classical books were 5
-                      euro.</p>
+                      euro."</p>
                       <a target="_blank" href="https://www.google.com/search?sa=X&sca_esv=5aa8bd575662fb0a&rlz=1C1GCEA_enNL980NL983&hl=tr-NL&tbm=lcl&sxsrf=ADLYWIKaM-rkdmeukbTCPwOr8gKV28yFPw:1716680700758&q=le%20flaneur%20amsterdam%20yorumlar%C4%B1&rflfq=1&num=20&stick=H4sIAAAAAAAAAONgkxI2NjM1NzQwNrCwMDU1tTAyNbM02cDI-IpRPidVIS0nMS-1tEghMbe4JLUoJTFXoTK_qDQ3J7HoyMZFrIRUAACNOfqfXgAAAA&rldimm=3657103088555825694&ved=0CEIQ5foLahcKEwjY9rXl_qmGAxUAAAAAHQAAAAAQCA&biw=1775&bih=917&dpr=1#lkt=LocalPoiReviews&arid=ChdDSUhNMG9nS0VJQ0FnSUM1b2VYendBRRAB">
                       Polina Vasilyeva
                     </a>
@@ -107,12 +107,12 @@ const Testimonials: React.FC = () => {
               <div className="testimonial-carousel-caption carousel-caption  d-md-block">
               <div className="col-md-8 offset-md-2">
                 <div className="testimonial-card">
-                    <p className="quote">Nice second hand bookshop. It has a very smart
+                    <p className="quote">"Nice second hand bookshop. It has a very smart
                       organisation in terms of languages: English books on the
                       right and Dutch on the left, and within this division many
                       topics. The shop is bigger than it looks, and it has a
                       nice spot at the end with place to sit and some art
-                      prints.</p>
+                      prints."</p>
                       <a target="_blank" href="https://www.google.com/search?sa=X&sca_esv=5aa8bd575662fb0a&rlz=1C1GCEA_enNL980NL983&hl=tr-NL&tbm=lcl&sxsrf=ADLYWIKaM-rkdmeukbTCPwOr8gKV28yFPw:1716680700758&q=le%20flaneur%20amsterdam%20yorumlar%C4%B1&rflfq=1&num=20&stick=H4sIAAAAAAAAAONgkxI2NjM1NzQwNrCwMDU1tTAyNbM02cDI-IpRPidVIS0nMS-1tEghMbe4JLUoJTFXoTK_qDQ3J7HoyMZFrIRUAACNOfqfXgAAAA&rldimm=3657103088555825694&ved=0CCkQ5foLahcKEwiI--Hl_amGAxUAAAAAHQAAAAAQCA&biw=1775&bih=917&dpr=1#lkt=LocalPoiReviews&arid=ChdDSUhNMG9nS0VJQ0FnSUNENU52Vzh3RRAB">
                       R. Sabater
                     </a>
