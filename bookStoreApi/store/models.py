af8ca030b10df7fa98bac71523e04d4aabb5e5f2
unknown <hasankaradirek3@gmail.com>
2024-10-24 00:33:38 +0200

    created_at field added to order model, order admin updated

--- bookStoreApi/store/models.py
+++ bookStoreApi/store/models.py
@@ -62,6 +62,7 @@ class Order(models.Model):
     post_cost = models.DecimalField(
         max_digits=10, decimal_places=2, default=Decimal("0.00")
     )
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
 
     def __str__(self):
         return f"Order {self.id} "
