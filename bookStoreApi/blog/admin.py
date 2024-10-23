8e786e7716c951391b0e581174913b3acb2a19e8
unknown <hasankaradirek3@gmail.com>
2024-10-24 00:24:45 +0200

    post and form admin list_display updated

--- bookStoreApi/blog/admin.py
+++ bookStoreApi/blog/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from .models import Post, Form, FormImage
 
-admin.site.register(Post)
-admin.site.register(Form)
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'content', 'created_at')
+class FormAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'created_at') 
+admin.site.register(Form,FormAdmin)
+admin.site.register(Post,PostAdmin)
 admin.site.register(FormImage)
