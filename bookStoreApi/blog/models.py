76f5776a24bdb53a901ddb8e55e881e2462df8d7
unknown <hasankaradirek3@gmail.com>
2024-10-24 00:04:33 +0200

    created_at field added to Post and Form tables

--- bookStoreApi/blog/models.py
+++ bookStoreApi/blog/models.py
@@ -1,10 +1,13 @@
 from django.db import models
+from django.utils import timezone
+
 
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField()
     image = models.ImageField(upload_to="book_images/")
+    created_at = models.DateTimeField(default=timezone.now, auto_now_add=True, null=True)
 
     def __str__(self):
         return self.title
@@ -14,6 +17,7 @@ class Form(models.Model):
     name = models.CharField(max_length=255)
     email = models.EmailField(blank=False, null=False)
     message = models.TextField()
+    created_at = models.DateTimeField(default=timezone.now, auto_now_add=True, null=True)
 
     def __str__(self):
         return self.name
