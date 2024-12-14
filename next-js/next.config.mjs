51b477af4f9c177747eeb9efb2dae997bb5745a0
HasanK <hasankaradirek3@gmail.com>
2024-12-14 11:59:00 +0100

    eslint dsabled on production

--- next-js/next.config.mjs
+++ next-js/next.config.mjs
@@ -3,6 +3,10 @@ const nextConfig = {
   images: {
     domains: ["bookstorestatic.s3.eu-north-1.amazonaws.com"],
   },
+  eslint: {
+    // Disable ESLint during builds
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
