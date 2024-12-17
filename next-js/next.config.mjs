544e1261e9fda5781eefab6c918d68a65b8e051e
HasanK <hasankaradirek3@gmail.com>
2024-12-17 21:06:18 +0100

    nextjs image url bug

--- next-js/next.config.mjs
+++ next-js/next.config.mjs
@@ -1,7 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["storage.googleapis.com","leflaneuramsterdam.com"],
+    domains: ["storage.googleapis.com", "leflaneuramsterdam.com"],
+
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "storage.googleapis.com",
+        pathname: "/leflaneur_bucket/**", // Match signed Google Storage URLs
+      },
+      {
+        protocol: "https",
+        hostname: "leflaneuramsterdam.com",
+        pathname: "/static/book_images/**", // Nginx-proxied static paths
+      },
+    ],
   },
   eslint: {
     // Disable ESLint during builds
